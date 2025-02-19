616ed935382a5ba9300935c7e823ab4fcd7ddd8a
Nandini Vishnoi <nandinivishnoi2016@gmail.com>
2025-02-19 17:29:39 +0530

    Create constructor.py

---
+++ OPPs/constructor.py
@@ -0,0 +1,15 @@
+class student:
+    def __init__(self,fullname):
+        self.name=fullname
+        print("Adding new student to database")
+
+s1=student("Nandini")
+print(s1.name)
+s2=student("Manish")
+print(s2.name)
+s3=student("Vishank")
+print(s3.name)
+s4=student("Khushbu")
+print(s4.name)
+s5=student("Lovely")
+print(s5.name)
