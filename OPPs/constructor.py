9f934950cdd259fab7142afbf18328ee02bc91c3
Nandini Vishnoi <nandinivishnoi2016@gmail.com>
2025-02-19 17:39:58 +0530

    added marks

--- OPPs/constructor.py
+++ OPPs/constructor.py
@@ -1,15 +1,16 @@
 class student:
-    def __init__(self,fullname):
+    def __init__(self,fullname,marks):
         self.name=fullname
+        self.marks=marks
         print("Adding new student to database")
 
-s1=student("Nandini")
-print(s1.name)
-s2=student("Manish")
-print(s2.name)
-s3=student("Vishank")
-print(s3.name)
-s4=student("Khushbu")
-print(s4.name)
-s5=student("Lovely")
-print(s5.name)
+s1=student("Nandini",96)
+print(s1.name,s1.marks)
+s2=student("Manish",97)
+print(s2.name,s2.marks)
+s3=student("Vishank",87)
+print(s3.name,s3.marks)
+s4=student("Khushbu",85)
+print(s4.name,s4.marks)
+s5=student("Lovely",79)
+print(s5.name,s5.marks)
